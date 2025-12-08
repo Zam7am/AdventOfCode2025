2da122b3640b886d84c6f80e5c90e14fb6331892
Zam7am <zam7am@pm.me>
2025-12-08 23:48:56 +0100

    wip for day 8 part 1 - missing final logic to count connections

--- day8/part1.hs
+++ day8/part1.hs
@@ -1,67 +1,81 @@
 import Data.Either (fromRight)
 import Data.Function (on)
-import Data.List
+import Data.List as List
+import Data.Maybe (fromJust)
 import Data.Ord
-import Data.Text (pack, splitOn)
+import Data.Sequence as Seq
+import Data.Set qualified as Set
+import Data.Text as Text (pack, splitOn)
 import Data.Text.Read qualified as R
-
--- NOTE: TOO slow
--- Solution 1: Try better Data structure (probably still not better)
--- Solution 2: For each circuit, keep track of the minimal distance of each other circuit
---  -> initial computation + for each connect you need to update for each circuit
+import Debug.Trace (trace)
 
 main = do
   file <- readFile "test.txt"
-  let circuits = parseInput file
-  print $ multThreeLargestCircuits $ connect 10 circuits
+  let junctionBoxes = parseInput file
+  let connectedJunctionBoxes = connect junctionBoxes
+  print $ Seq.sortOn ((\(Closest _ dist) -> dist) . snd) $ fmap (\j -> (fst j, getClosest (snd j))) connectedJunctionBoxes
+  print $ connectPairs 10 connectedJunctionBoxes
 
--- print $ map length $ connect 4 circuits
+-- print $ multThreeLargestCircuits $ connect 10 circuits
 
-data JunctionBox = JunctionBox Int Int Int
-  deriving (Show, Eq, Ord)
-
-type Circuit = [JunctionBox]
-
-parseInput :: String -> [Circuit]
-parseInput string = map (toList . toJunctionBox) splitCoords
+connectPairs :: Int -> Boxes -> Edges
+connectPairs 0 _ = Set.empty
+connectPairs _ Seq.Empty = Set.empty
+connectPairs n (jb :<| jbs) = Set.insert (otherId, id) $ Set.insert (id, otherId) $ connectPairs (n - 1) jbs
  where
-  toList x = [x]
-  toJunctionBox [x, y, z] = JunctionBox (readInt x) (readInt y) (readInt z)
+  id = fst jb
+  (JunctionBox _ _ _ (Just (Closest otherId _))) = snd jb
+
+type ID = Int
+data Closest = Closest ID Float
+  deriving (Show, Eq)
+
+data JunctionBox = JunctionBox Int Int Int (Maybe Closest)
+  deriving (Show, Eq)
+
+type Boxes = Seq (ID, JunctionBox)
+type Edges = Set.Set (Int, Int)
+
+getClosest :: JunctionBox -> Closest
+getClosest (JunctionBox _ _ _ closest) = fromJust closest
+
+parseInput :: String -> Boxes
+parseInput string = fromList $ List.zip [0 ..] $ map toJunctionBox splitCoords
+ where
+  toJunctionBox [x, y, z] = JunctionBox (readInt x) (readInt y) (readInt z) Nothing
   splitCoords = map (splitOn delimiter) text
   text = map pack inputLines
   delimiter = pack ","
   inputLines = lines string
+  readInt x =
+    case R.decimal x of
+      Right (val, _) -> val
+      Left _ -> error "invalid number"
 
-readInt x =
-  case R.decimal x of
-    Right (val, _) -> val
-    Left _ -> error "invalid number"
+connect :: Boxes -> Boxes
+connect jbs = fmap (closestDistance jbs) jbs
 
-boxDistance :: JunctionBox -> JunctionBox -> Float
-boxDistance (JunctionBox x1 y1 z1) (JunctionBox x2 y2 z2) = sqrt $ xdiff ^ 2 + ydiff ^ 2 + zdiff ^ 2
+closestDistance :: Boxes -> (ID, JunctionBox) -> (ID, JunctionBox)
+closestDistance boxes (id, junctionbox) = updatedJunctionbox
+ where
+  (updatedJunctionbox, _) = connectJunctionBoxes (id, junctionbox) closestJb
+  closestJb = minimumBy (compare `on` (getDistance . snd)) jbs
+  jbs = Seq.filter (\j -> snd j /= junctionbox) boxes
+  getDistance = distance junctionbox
+
+connectJunctionBoxes :: (ID, JunctionBox) -> (ID, JunctionBox) -> ((ID, JunctionBox), (ID, JunctionBox))
+connectJunctionBoxes (id1, jb1) (id2, jb2) = (leftJb, rightJb)
+ where
+  rightJb = (id2, setClosest jb2 id1 dist)
+  leftJb = (id1, setClosest jb1 id2 dist)
+  dist = distance jb1 jb2
+
+setClosest :: JunctionBox -> ID -> Float -> JunctionBox
+setClosest (JunctionBox x y z maybeClosest) id dist = JunctionBox x y z (Just (Closest id dist))
+
+distance :: JunctionBox -> JunctionBox -> Float
+distance (JunctionBox x1 y1 z1 _) (JunctionBox x2 y2 z2 _) = sqrt $ xdiff ** 2 + ydiff ** 2 + zdiff ** 2
  where
   xdiff = fromIntegral $ x1 - x2
   ydiff = fromIntegral $ y1 - y2
   zdiff = fromIntegral $ z1 - z2
-
-distance :: Circuit -> Circuit -> Float
-distance circuitA circuitB = minimum distances
- where
-  distances = map (uncurry boxDistance) pairs
-  pairs = [(c1, c2) | c1 <- circuitA, c2 <- circuitB]
-
-multThreeLargestCircuits :: [Circuit] -> Int
-multThreeLargestCircuits circuits = product largest
- where
-  largest = (take 3 . sortBy (comparing Down) . map length) circuits
-
-connect :: Int -> [Circuit] -> [Circuit]
-connect 1 circuits = circuits -- connect nothing
-connect n circuits = connect (n - 1) $ connectCircuits circuits
-
--- use data structure with fast concatenation and replacing of values
-connectCircuits :: [Circuit] -> [Circuit]
-connectCircuits circuits = (closestA ++ closestB) : filter (\c -> c /= closestA && c /= closestB) circuits
- where
-  (closestA, closestB) = minimumBy (compare `on` uncurry distance) pairs
-  pairs = [(c1, c2) | c1 <- circuits, c2 <- circuits, c1 /= c2]
